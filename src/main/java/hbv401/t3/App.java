8827f202498713ffc5800d277b5cbfb1df5c861e
Skúli Þór Bjarnason <skuli.thor.bjarnason@gmail.com>
2020-03-15 15:39:31 +0000

    added Profilefilter to MapControl

--- src/main/java/hbv401/t3/App.java
+++ src/main/java/hbv401/t3/App.java
@@ -10,18 +10,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+// import javafx.scene.control.Label;
+// import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    // private static final Logger logger = LoggerFactory.getLogger(App.class);
     @Override
     public void start(final Stage primaryStage) throws Exception{
         final String fxmlFile = "/fxml/DemoApp.fxml";
@@ -45,7 +45,7 @@ public class App extends Application {
         final FXMLLoader fxmlLoader = new FXMLLoader();
         final Parent rootNode = fxmlLoader.load(getClass().getResourceAsStream(fxmlFile));
 
-        ///
+        //
         final MapController controller = fxmlLoader.getController();
         final Projection projection = getParameters().getUnnamed().contains("wgs84")
                 ? Projection.WGS_84 : Projection.WEB_MERCATOR;
--- src/main/java/hbv401/t3/MapController.java
+++ src/main/java/hbv401/t3/MapController.java
@@ -1,59 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hbv401.t3;
 import com.sothawo.mapjfx.*;
 import com.sothawo.mapjfx.event.MapLabelEvent;
-import com.sothawo.mapjfx.event.MapViewEvent;
-import com.sothawo.mapjfx.event.MarkerEvent;
-import com.sothawo.mapjfx.offline.OfflineCache;
-import javafx.animation.Transition;
-import javafx.beans.binding.Bindings;
-import javafx.beans.value.ChangeListener;
+// import com.sothawo.mapjfx.event.MapViewEvent;
+// import com.sothawo.mapjfx.event.MarkerEvent;
+// import com.sothawo.mapjfx.offline.OfflineCache;
 import javafx.fxml.FXML;
-import javafx.scene.control.Accordion;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TitledPane;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.util.Duration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Controller for the FXML defined code.
- *
- * @author P.J. Meisch (pj.meisch@sothawo.com).
- */
 public class MapController {
 
-    private Map<String, Marker> markersCreatedFromFeatures = new HashMap<>();
+    private Map<String, Marker> features = new HashMap<>();
 
     // private static final Logger logger = LoggerFactory.getLogger(MapController.class);
-    private static final Coordinate coIsland = new Coordinate(65.178, -17.919);
+    // private static final Coordinate coIsland = new Coordinate(65.178, -17.919);
 
     @FXML
     private MapView mapView;
 
     @FXML
-    private TextField origin;
+    public TextField tripFrom;
 
     @FXML
-    private TextField destination;
+    public TextField tripTo;
+
+    //public String[] profileFeatures = { "foss", "gos" };
+    public String[] profileFeatures;
 
     public MapController() {
 
@@ -79,27 +58,31 @@ public class MapController {
         mapView.addEventHandler(MapLabelEvent.MAPLABEL_CLICKED, event -> {
                 event.consume();
                 String orig = event.getMapLabel().getText();
-                origin.setText(orig);
+                tripFrom.setText(orig);
             });
             mapView.addEventHandler(MapLabelEvent.MAPLABEL_RIGHTCLICKED, event -> {
                 event.consume();
                 String dest = event.getMapLabel().getText();
-                destination.setText(dest);
+                tripTo.setText(dest);
             });
     }
- 
-    public void afterMapIsInitialized(List<MapFeature> mapFeatures){
+
+    private void afterMapIsInitialized(List<MapFeature> mapFeatures){
+        
         for (MapFeature mf : mapFeatures) {
-            final String str = mf.getCategory(); 
-            final String label = "/".concat(str).concat(".png");
-            final Coordinate cord = new Coordinate(mf.getCoordx(), mf.getCoordy());
-            final Marker marker = new Marker(getClass().getResource(label), -20, -20).setPosition(cord).setVisible(true);
-            marker.attachLabel(new MapLabel(mf.getName(),20,20));
-            mapView.addMarker(marker);
-            markersCreatedFromFeatures.put(marker.getId(), marker);
+            if (Arrays.asList(profileFeatures).contains(mf.getCategory())){
+                    final String str = mf.getCategory();
+                    final String label = "/".concat(str).concat(".png");
+                    final Coordinate cord = new Coordinate(mf.getCoordx(), mf.getCoordy());
+                    final Marker marker = new Marker(getClass().getResource(label), -20, -20).setPosition(cord).setVisible(true);
+                    marker.attachLabel(new MapLabel(mf.getName(),20,20));
+                    mapView.addMarker(marker);
+                    features.put(marker.getId(), marker);
+            }
         }
         mapView.setZoom(6);
-        mapView.setCenter(coIsland);
+        //Island
+        mapView.setCenter(new Coordinate(65.178, -17.919));
     }
 
 }
