ddbe42db53f9fc88ef58af22b33c92dda11da99d
sthb13 <42750168+sthb13@users.noreply.github.com>
2020-03-15 01:16:45 +0000

    cleanup

--- src/main/java/hbv401/t3/App.java
+++ src/main/java/hbv401/t3/App.java
@@ -1,8 +1,11 @@
 package hbv401.t3;
 
-import com.sothawo.mapjfx.Projection;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.sothawo.mapjfx.Projection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,10 +13,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.*;
+
 
 /**
  * JavaFX App
@@ -22,34 +24,24 @@ public class App extends Application {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
     @Override
     public void start(final Stage primaryStage) throws Exception{
-        // logger.info("starting DemoApp");
         final String fxmlFile = "/fxml/DemoApp.fxml";
-        // logger.debug("loading fxml file {}", fxmlFile);
-        // logger.trace("stage loaded");
 
         //initialize mapfeatures
 
         final File text = new File("src/main/resources/features.txt");
         final Scanner input = new Scanner(text);
         input.useDelimiter(";|\n");
-        //Local locale = Locale.IsoCountryCode(ISL);
-        //input.useLocale(locale);
         List<MapFeature> mapFeatures = new ArrayList<>();
-
         while (input.hasNext()) {
-
             final String name = input.next();
             final String category = input.next();
             final double x = input.nextDouble();
             final double y = input.nextDouble();
-            System.out.println("********** " + name); 
             mapFeatures.add(new MapFeature(name, category, x, y));
         }
         input.close();
-        // System.out.println(mapFeatures[0]); 
 
-
-
+        //initialize fxml
         final FXMLLoader fxmlLoader = new FXMLLoader();
         final Parent rootNode = fxmlLoader.load(getClass().getResourceAsStream(fxmlFile));
 
@@ -57,26 +49,17 @@ public class App extends Application {
         final MapController controller = fxmlLoader.getController();
         final Projection projection = getParameters().getUnnamed().contains("wgs84")
                 ? Projection.WGS_84 : Projection.WEB_MERCATOR;
-        // controller.
         controller.initMapAndControls(projection,mapFeatures);
 
         final Scene scene = new Scene(rootNode);
-        // logger.trace("scene created");
 
         primaryStage.setTitle("HBV401 T3");
         primaryStage.setScene(scene);
-        // logger.trace("showing scene");
         primaryStage.show();
-
-        // logger.debug("application start method finished.");
 
     }
 
-    public static void main(final String[] args) throws FileNotFoundException{
-
-       // Scanner input = new Scanner(new File("features.txt"));
-        //input.useDelimiter(";|\n");
-        // System.out.println(input);
+    public static void main(String[] args) {
         launch();
     }
 
